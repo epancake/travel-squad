545ebdfb42aa8efa4b11d42fec275b9f5d21d4e5
Emily Pancake <epancake24@gmail.com>
2018-03-21 09:21:12 -0600

    add group name data field

--- migrations/20180316142139_groups.js
+++ migrations/20180316142139_groups.js
@@ -3,7 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("groups", table => {
     table.increments("id").primary();
     table.text("url");
-  })
+    table.text("name");
+  });
 };
 
 exports.down = function(knex, Promise) {
--- seeds/data01.js
+++ seeds/data01.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('groups').del()
+  return knex("groups").del()
     .then(function () {
       // Inserts seed entries
-      return knex('groups').insert([
-        {id: 1, url: '123456789'},
-        {id: 2, url: '234567890'},
-        {id: 3, url: '345678901'}
+      return knex("groups").insert([
+        {id: 1, url: "123456789", name:"The Wilsons in Miami"},
+        {id: 2, url: "234567890", name:"Mark turns 40"},
+        {id: 3, url: "345678901", name:"Team Colombia"}
       ]);
     });
 };

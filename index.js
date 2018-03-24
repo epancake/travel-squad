6c70e8da18c0194a6c3e102d3117ee1abb0dceec
Emily Pancake <epancake24@gmail.com>
2018-03-24 17:46:20 -0600

    react scripts start

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const app = require("./app");
+const io = require('socket.io')();
 
-app.listen(process.env.PORT || 3000);
+// io.on('connection', (client) => {
+//   // here you can start emitting events to the client
+// });
+
+let port = process.env.PORT || 3000
+
+// io.listen(port);
+console.log('listening on port ', port);
+
+app.listen(port);
